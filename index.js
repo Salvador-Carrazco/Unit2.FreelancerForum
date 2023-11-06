6210a95199783b20d0546d2860c50952770d845b
kingdapster <sal.carrazco89@gmail.com>
2023-11-06 13:47:16 -0800

    i need more work with this

---
+++ index.js
@@ -0,0 +1,31 @@
+const freelnacers = [
+{ name: `Alice`, occupation: `Writer`, price: 30},
+{ name: `Bob`, occupation: `Teacher`, price: 50},
+{ name: `Carol`, occupation: `Programmer`, price: 70},
+{ name: `John`,  occupation: `labor`, price: 95},
+{ name: `Salvaodr`,  occupation: `Artist`, price:120},
+
+];
+
+const body = document.body
+const header = document.createElement('h1')
+const header2 = document.createElement(`h3`).style.fontFamily = `san-serif`
+const header3 = document.createElement(`h2`)
+const headername = document.createElement(`h3`)
+const headerOC =  document.createElement(`h3`)
+const headerPrice = document.createElement(`h3`)
+
+
+header.textContent = "Welcome to the Freelancer Forum"
+header.setAttribute("style", "text-align:center")
+body.appendChild(header)
+header.style.borderStyle = `solid`
+
+
+header2.textContent = `The average starting price is `
+
+body.element.appendChild(header2)
+
+
+
+ 
